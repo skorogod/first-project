cfc71cd8715cc5d4a7051a84082cebb36fd6df16
sergey <skorogod123@yandex.ru>
2024-12-08 23:10:03 +0300

    feat: add personalized greeting

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello world')
+const prompt = require('prompt-sync')()
+
+let name = prompt("Enter your name:");
+
+while (true) {
+    if (name.trim()) {
+        console.log(`Hello ${name}`)
+        break
+    } else {
+        console.log("Bad name! Please, try again")
+        name = prompt("Enter your name:")
+    }
+}
